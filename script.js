7527e391fcba98bdd9d35ef9bfd5ed7d752cf0af
lucianatux <lucianatux@yahoo.com.ar>
2023-09-27 18:08:31 -0300

    piedrazo

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ const firstSoundsGroup = [
       keyCode: 69,
       key: 'E',
       description: 'Vidrios rotos',
-      url: './assets/piedrazoalaventana.mp3'
+      url: './otros/piedrazoalaventana.mp3'
     },
     {
       id:4,
